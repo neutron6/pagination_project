9f84f81bd10bd98217de9dcbbd6a0d451edc906f
rushi <rushikeshnichit@gmail.com>
2023-05-10 18:29:00 +0530

    done

---
+++ pagination_with_sorting/src/main/java/com/rsn/pagination/pagination_with_sorting/PaginationWithSortingApplication.java
@@ -0,0 +1,13 @@
+package com.rsn.pagination.pagination_with_sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationWithSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationWithSortingApplication.class, args);
+	}
+
+}
---
+++ pagination_with_sorting/src/test/java/com/rsn/pagination/pagination_with_sorting/PaginationWithSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsn.pagination.pagination_with_sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationWithSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
